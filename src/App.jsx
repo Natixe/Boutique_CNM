dd093dbedddfc76c4676e8b660e07f171f7fc0f9
Natixe <natixe28@gmail.com>
2024-05-27 00:20:41 +0200

    Version qui fonctionne 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
-import image4 from './images/image4.jpg';
-import image5 from './images/image5.jpg';
-import image6 from './images/image6.jpg';
-import "./styles.css";
+import * as React from "react";
+import { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import "./index.css";
+
+
+
+/*Images Import */
+import LogoStartMid from './assets/LogoStartMid.svg';
+import LogoApresL_intro from './assets/LogoApresL_intro.svg';
+import LogoCNM from './assets/LogoCNM.svg';
+import RectangleFormation1 from './assets/qfdswx.png';
 
 
 const rectangles = [
-  { id: 1, color: 'red', blur: false },
-  { id: 2, color: 'blue', blur: true },
-  { id: 3, color: 'magenta', blur: true },
-  { id: 4, color: 'cyan', blur: true },
-  { id: 5, color: 'yellow', blur: true },
-  { id: 6, color: 'green', blur: true },
-  { id: 7, color: 'orange', blur: true },
-  { id: 8, color: 'purple', blur: true },
+  { id: 1, image: RectangleFormation1, blur: true },
+  { id: 2, image: RectangleFormation1, blur: true },
+  { id: 3, image: RectangleFormation1, blur: true },
+  { id: 4, image: RectangleFormation1, blur: true },
+  { id: 5, image: RectangleFormation1, blur: true },
+  { id: 6, image: RectangleFormation1, blur: true },
+  { id: 7, image: RectangleFormation1, blur: true },
+  { id: 8, image: RectangleFormation1, blur: false },
 ];
 
 const positions = [
@@ -31,8 +34,52 @@ const positions = [
   { top: '62.96%', left: '44.98%' },
 ];
 
+
 function App() {
+  const [count, setCount] = useState(0)
+  const [showRectagleStart, setshowRectagleStart] = useState(true);
+  const [showLogoStartMid, setshowLogoStartMid] = useState(true);
+  const [showLogoApresL_intro, setshowLogoApresL_intro] = useState(false);
+  const [showAnimeLogoLeftUpset, setshowAnimeLogoLeftUpset] = useState(false);
+  const [showLogoLeftUpset, setshowLogoLeftUpset] = useState(false);
+  const [showFormationRectangle, setshowFormationRectangle] = useState(false);
   const [items, setItems] = useState(rectangles);
+
+  
+  useEffect(() => {
+    
+    /*Supprime les rectangle de l'intro*/
+    const timerRectagle = setTimeout(() => {
+      setshowRectagleStart(false);
+    }, 1955);
+
+    /*Supprime le logo au tout debut au centre*/
+    const timerLogoStartMid = setTimeout(() => {
+      setshowLogoStartMid(false);
+    }, 1400);
+
+    /*Ajoute le gros logo au centre*/
+    const timerLogoApresL_intro = setTimeout(() => {
+      setshowLogoApresL_intro(true);
+    }, 1900);
+
+    /*Ajoute le Logo cnm en haut a gauche puis Supprime le logo CNM en haut a gauche*/
+    const timerLogoAnimeLeftUp = setTimeout(() => {
+      setshowAnimeLogoLeftUpset(true);
+    }, 2000);
+
+    /*Ajoute le Logo cnm en haut a gauche puis Supprime le logo CNM en haut a gauche*/
+    const timerLogoLeftUp = setTimeout(() => {
+      setshowAnimeLogoLeftUpset(false);
+      setshowLogoLeftUpset(true);
+    }, 3300);
+
+    const FormationRectangle = setTimeout(() => {
+      setshowFormationRectangle(true);
+    }, 3300);
+    
+    return () => clearTimeout(timerRectagle,timerLogoStartMid,timerLogoApresL_intro,timerLogoAnimeLeftUp,timerLogoLeftUp,FormationRectangle);
+  }, []); 
 
   const handleRightClick = () => {
     setItems(prev => {
@@ -53,37 +100,166 @@ function App() {
       }));
     });
   };
+
   return (
     <>
-      <div className="container">
-        <button className="arrow arrow-left" onClick={handleLeftClick}>◀</button>
-
-        <AnimatePresence>
-          {items.map((item, index) => (
-            <motion.div
-              key={item.id}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1, top: positions[index].top, left: positions[index].left }}
+    
+      {/*Logo au centre au tout debut qui va vers le bas*/}
+      {showLogoStartMid && (
+        <div className="LogoCnmStartContainer">
+          <motion.img
+          initial={{ translateY: '0%' }}
+          animate={{ translateY: '140%' }}
+          transition={{ 
+            duration: 0.6,
+            delay: 0.7,
+            ease: [0.62, 0, 0.36, 1]
+          }}
+          className="LogoCnmStart"
+          src={LogoStartMid}>
+          </motion.img>
+        </div>  
+      )}
+      {/*----------------------------------------------*/}        
+      {/*Animation des rectangle au debut*/}
+        {showRectagleStart && (
+          <div>
+            {/*Animation du rectangle qui va vers le haut*/}
+            <motion.div 
+              className="RectangleStart BoxStartUp"
+              initial={{ y: "0%", opacity: 1 }}
+              animate={{ y: "-100%", opacity: 1 }}
               exit={{ opacity: 0 }}
               transition={{ 
-                type: "spring", 
-                stiffness: 100, 
-                damping: 10 }}
-              className="rectangle"
-              style={{
-                backgroundColor: item.color,
-                filter: item.blur ? 'blur(5px)' : 'none',
+                duration: 0.6,
+                delay: 1.4,
+                ease: [1, 0.0, 0.5, 1.0]
+              }}
+            />          
+            {/*Animation du rectangle qui va vers le bas*/}
+            <motion.div className="RectangleStart BoxStartDown"
+              initial={{ y: "0%", opacity: 1 }}
+              animate={{ y: "100%", opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{
+                duration: 0.6,
+                delay: 1.4,
+                ease: [1, 0.0, 0.5, 1.0]
+              }}
+            />
+          </div>
+        )}
+      {/*----------------------------------------------*/}
+
+      {/*Logo Centré apres l'intro du site*/}
+      {showLogoApresL_intro && (
+        <div className="LogoCnmAfterStartContainer">
+          <motion.div
+              className="BlurLogoAfterStart"
+              initial={{ filter: "blur(0px)"}}
+              animate={{ filter: "blur(3.5px)", opacity: 0.4}}
+              transition={{
+                duration: 1.7,
+                delay: -0.5,
+                ease: [0.66, 0, 0.78, 0.9]
               }}
             >
-              {item.id}
+              <motion.img 
+              className="LogoCnmAfterStart"
+              src={LogoApresL_intro}
+              initial={{ opacity: 0, scale: 1, y: "-2.2%"}}
+              animate={{ opacity: 0.8, scale: 0.8, y: "-2.2%"}}
+              transition={{
+                duration: 1.2,
+                delay: -0.3,
+                ease: [0.37, 0, 0.37, 1]
+              }}
+              />
             </motion.div>
-          ))}
-        </AnimatePresence>
+          </div>
+        )}
+        {/*----------------------------------------------*/}
 
-        <button className="arrow arrow-right" onClick={handleRightClick}>▶</button>
-      </div>
+        {/*Intro Nav Bar*/}
+        {showAnimeLogoLeftUpset && (        
+          <nav className="nav w-clearfix">
+            <div 
+              className="hero-link w-inline-block w--current"
+              >
+                <motion.img 
+                  className="CNM-logo"
+                  initial={{ y: "-250%"}}
+                  animate={{ y: "0%"}}
+                  src={LogoCNM} 
+                  alt="" 
+                  transition={{ 
+                    duration: 0.6,
+                    delay: 0.7,
+                    ease: [0, 0, 0.5, 1.0]
+                  }}
+                />
+            </div>
+          </nav>
+        )}
+        {/*Nav Bar Apres intro*/}
+        {showLogoLeftUpset && (        
+          <nav className="nav w-clearfix">
+            <div 
+              className="hero-link w-inline-block w--current"
+              > 
+                <motion.a
+                href="#"
+                className="ButtonLogoCNMLeftUp"
+                >
+                  <motion.img 
+                    className="CNM-logo" 
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.9 }}
+                    transition={{ 
+                      type: "spring", 
+                      stiffness: 400, 
+                      damping: 17 }}
+                    src={LogoCNM} 
+                    alt="" 
+                  />
+                </motion.a>                  
+            </div>
+          </nav>
+        )}
+      {/* --------------------------------------- */}
+
+      {/* Rectangle formation mid */}
+      {showFormationRectangle && (
+        <div className="containerFormationRectangle">
+          <button className="arrow arrow-left" onClick={handleLeftClick}>◀</button>
+          <AnimatePresence>
+            {items.map((item, index) => (
+              <motion.div
+                className="FormationRectangle"
+                key={item.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1, top: positions[index].top, left: positions[index].left }}
+                exit={{ opacity: 0 }}
+                transition={{ 
+                  type: "spring", 
+                  stiffness: 100, 
+                  damping: 10 
+                }}
+                style={{
+                  backgroundImage: `url(${item.image})`,
+                  filter: item.blur ? 'blur(5px)' : 'none',
+                  backgroundSize: 'cover',
+                }}
+              >
+                {item.id}
+              </motion.div>
+            ))}
+          </AnimatePresence>
+          <button className="arrow arrow-right" onClick={handleRightClick}>▶</button>
+        </div>
+      )}
     </>
-  );
+  )
 }
 
 export default App
